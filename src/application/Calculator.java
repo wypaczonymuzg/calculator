b21a5eca9c2bca79cd386fe141fecc2ccc69a72d
mozg <mozg@localhost.localdomain>
2014-05-22 01:15:26 +0200

    added math functions

--- src/application/Calculator.java
+++ src/application/Calculator.java
@@ -1,15 +1,125 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Iterator;
+
+import org.apache.commons.math3.analysis.function.Acosh;
+import org.apache.commons.math3.analysis.function.Asinh;
+import org.apache.commons.math3.analysis.function.Atanh;
+import org.apache.commons.math3.util.MathUtils;
+
 import de.congrace.exp4j.Calculable;
+import de.congrace.exp4j.CustomFunction;
+import de.congrace.exp4j.CustomOperator;
 import de.congrace.exp4j.ExpressionBuilder;
+import de.congrace.exp4j.InvalidCustomFunctionException;
 import de.congrace.exp4j.UnknownFunctionException;
 import de.congrace.exp4j.UnparsableExpressionException;
 
 public class Calculator {
+	//private static Collection<CustomFunction> cusFunctions;
+
+	  private static Collection<CustomFunction> cusFunctions = new ArrayList<CustomFunction>();
+	  private static Collection<CustomOperator> cusOperators = new ArrayList<CustomOperator>();
+	
+	
+	
+	public static void initFunctions() {
+		
+		//cusFunctions = new Collection<CustomFunction>();
+		
+		CustomOperator mod = new CustomOperator("%", true, 3) {
+			@Override
+			protected double applyOperation(double[] values) {
+				if (values[1] == 0d) {
+					throw new ArithmeticException("Division by zero!");
+				}
+				return values[0] % values[1];
+			}
+		};
+		CustomOperator factorial = new CustomOperator("!", true, 6, 1) {
+			@Override
+			protected double applyOperation(double[] values) {
+				double tmp = 1d;
+				int steps = 1;
+				while (steps < values[0]) {
+					tmp = tmp * (++steps);
+				}
+				return tmp;
+			}
+		};
+		try {
+			CustomFunction max = new CustomFunction("max", 2) {
+				@Override
+				public double applyFunction(double[] values) {
+					double max = values[0];
+					for (int i = 1; i < this.getArgumentCount(); i++) {
+						if (values[i] > max) {
+							max = values[i];
+						}
+					}
+					return max;
+				}
+			};
+			CustomFunction min = new CustomFunction("min", 2) {
+				@Override
+				public double applyFunction(double[] values) {
+					double min = values[0];
+					for (int i = 1; i < this.getArgumentCount(); i++) {
+						if (values[i] < min) {
+							min = values[i];
+						}
+					}
+					return min;
+				}
+			};
+			CustomFunction round = new CustomFunction("round") {
+				public double applyFunction(double[] values) {
+					return Math.round(values[0]);
+				}
+			};
+			CustomFunction truncate = new CustomFunction("trunc", 2) {
+				public double applyFunction(double[] values) {
+					return Math.floor(values[0]);
+				}
+			};
+			CustomFunction acosh = new CustomFunction("acosh") {
+				public double applyFunction(double[] values) {
+					return new Acosh().value(values[0]);
+				}
+			};
+			CustomFunction asinh = new CustomFunction("asinh") {
+				public double applyFunction(double[] values) {
+					return new Asinh().value(values[0]);
+				}
+			};
+			CustomFunction atanh = new CustomFunction("atanh") {
+				public double applyFunction(double[] values) {
+					return new Atanh().value(values[0]);
+				}
+			};
+			
+			cusFunctions.add(atanh);
+			cusFunctions.add(asinh);
+			cusFunctions.add(acosh);
+			cusFunctions.add(truncate);
+			cusFunctions.add(round);
+			cusFunctions.add(min);
+			cusFunctions.add(max);
+		} catch (InvalidCustomFunctionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
 	public static double calculate(String form, String var, double x)
 			throws UnknownFunctionException, UnparsableExpressionException {
+		form = form.replaceAll("PI", "3.14159");
+		form = form.replaceAll("E", "2.718281");
+
 		Calculable calc = new ExpressionBuilder(form).withVariable(var, x)
-				.build();
+				.withCustomFunctions(cusFunctions).build();
 
 		return calc.calculate();
 	}
--- src/application/Controller.java
+++ src/application/Controller.java
@@ -2,7 +2,9 @@ package application;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.ResourceBundle;
 
 import javax.imageio.ImageIO;
 
@@ -10,6 +12,7 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.embed.swing.SwingFXUtils;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.SnapshotParameters;
 import javafx.scene.chart.LineChart;
 import javafx.scene.chart.XYChart;
@@ -27,7 +30,13 @@ import de.congrace.exp4j.ExpressionBuilder;
 import de.congrace.exp4j.UnknownFunctionException;
 import de.congrace.exp4j.UnparsableExpressionException;
 
-public class Controller {
+public class Controller implements Initializable{
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Calculator.initFunctions();
+	}
+	
+	
 	@FXML
 	private Button btnCalculate;
 	@FXML
@@ -45,19 +54,22 @@ public class Controller {
 
 	ArrayList<Function> functionList = new ArrayList<>();
 	Function temp;
-	int edit=0;
+	int edit = 0;
+
 	@FXML
-	public void handleMouseClick(MouseEvent arg0) {	
-		edit=1;
-		temp = funListView.getSelectionModel().getSelectedItem();
-		txtFormula.setText(funListView.getSelectionModel().getSelectedItem()
-				.getExpression());
-		txtDomainFrom.setText(Double.toString(funListView.getSelectionModel()
-				.getSelectedItem().getFrom()));
-		txtDomainTo.setText(Double.toString(funListView.getSelectionModel()
-				.getSelectedItem().getTo()));
-		txtStep.setText(Double.toString(funListView.getSelectionModel()
-				.getSelectedItem().getStep()));
+	public void handleMouseClick(MouseEvent arg0) {
+		if (funListView.getSelectionModel().getSelectedItem() != null) {
+			edit = 1;
+			temp = funListView.getSelectionModel().getSelectedItem();
+			txtFormula.setText(funListView.getSelectionModel()
+					.getSelectedItem().getExpression());
+			txtDomainFrom.setText(Double.toString(funListView
+					.getSelectionModel().getSelectedItem().getFrom()));
+			txtDomainTo.setText(Double.toString(funListView.getSelectionModel()
+					.getSelectedItem().getTo()));
+			txtStep.setText(Double.toString(funListView.getSelectionModel()
+					.getSelectedItem().getStep()));
+		}
 	}
 
 	@FXML
@@ -95,43 +107,55 @@ public class Controller {
 	@FXML
 	private void btCalculate() throws UnknownFunctionException,
 			UnparsableExpressionException {
-			String formula, sFrom, sTo, sStep;
-			double from, to, step;
+		String formula, sFrom, sTo, sStep;
+		double from, to, step;
 
-			sFrom = txtDomainFrom.getText();
-			sTo = txtDomainTo.getText();
-			sStep = txtStep.getText();
+		sFrom = txtDomainFrom.getText();
+		sTo = txtDomainTo.getText();
+		sStep = txtStep.getText();
 
-			// lc.setTitle("hmmmm, :DD");
+		// lc.setTitle("hmmmm, :DD");
 
-			from = Double.parseDouble(sFrom);
-			to = Double.parseDouble(sTo);
-			step = Double.parseDouble(sStep);
-			formula = txtFormula.getText();
-			if (formula == null) {
-				System.out.println("formula null lol");
-			} else {
-				//System.out.println("formula  = "+formula+"\t from = "+from+"\t to = "+to+"\t step = "+step);
-				if (edit == 1) {
-					//System.out.println("edit mode!");
-					functionList.remove(temp);				
-					edit = 0;					
-				}
-				Function fun = new Function(formula, from, to, step);
-				functionList.add(fun);
-				lc.getData().clear();
-				ObservableList<Function> items = FXCollections
-						.observableArrayList(functionList);
-
-				funListView.setItems(items);
+		from = Double.parseDouble(sFrom);
+		to = Double.parseDouble(sTo);
+		step = Double.parseDouble(sStep);
+		formula = txtFormula.getText();
+	
+		
+		
+			// System.out.println("formula  = "+formula+"\t from = "+from+"\t to = "+to+"\t step = "+step);
+			if (edit == 1) {
+				// System.out.println("edit mode!");
+				functionList.remove(temp);
+				edit = 0;
+			}
+			Function fun;
+			try{
+				fun = new Function(formula, from, to, step);
+			}
+			catch(UnknownFunctionException | UnparsableExpressionException e){
 				
-				for (int i = 0; i < functionList.size(); i++) {
-					lc.getData().add(functionList.get(i).getSeries());
-				}
+		      e.printStackTrace();
+		      txtFormula.setStyle("-fx-border-color: red;");
+				return;
+			}
+		      txtFormula.setStyle("-fx-border-color: blue;");
+			 
+		
+			functionList.add(fun);
+			lc.getData().clear();
+			ObservableList<Function> items = FXCollections
+					.observableArrayList(functionList);
 
+			funListView.setItems(items);
+
+			for (int i = 0; i < functionList.size(); i++) {
+				lc.getData().add(functionList.get(i).getSeries());
 			}
 
-		}
+		
+
 	}
 
-
+	
+}
